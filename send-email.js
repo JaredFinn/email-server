2682c9ef2ed32cef8050839207b6494fc2d0748b
Jared Finn <jaredhfinn@gmail.com>
2023-04-04 21:45:13 -0400

    add cors

--- send-email.js
+++ send-email.js
@@ -8,6 +8,13 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 // Enable CORS for all requests
 app.use(cors());
 
